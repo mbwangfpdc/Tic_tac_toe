c279ca42a314ccafff67747bbdfd52c0b6ef9d08
Morgan Borjigin-Wang <mbwang10@gmail.com>
2017-12-20 23:50:24 -0500

    Finished likely
    Aggressive computer, should be unbeatable.

--- Files/Driver.py
+++ Files/Driver.py
@@ -14,7 +14,6 @@ class Tile:
 
 class Game:
     __board = [[Tile() for x in range(3)] for y in range(3)]
-    __is_x_turn = True
 
     def __init__(self):
         self.__is_x_turn = True
@@ -55,7 +54,10 @@ class Game:
                     return False
         return True
 
+    # Makes a move on the board - NOTE: 0-8, although interface is 1-9
     def __make_move(self, num_in, is_x):
+        while num_in < 0 or num_in > 8:
+            num_in = int(raw_input("Invalid input")) - 1
         while self.__id_at(num_in) != ' ':
             num_in = int(raw_input("That square is filled")) - 1
         if is_x:
@@ -95,13 +97,13 @@ class Game:
                 if self.__is_x_turn:
                     self.__make_move(int(raw_input("X's turn to move:")) - 1, self.__is_x_turn)
                 else:
-                    self.__make_ai_move2(self.__is_x_turn)
+                    self.__make_ai_move3(self.__is_x_turn)
                 self.__is_x_turn = not self.__is_x_turn
                 self.print_board()
         elif player_char == 'O':
             while not self.__someone_won() and not self.__stalemate():
                 if self.__is_x_turn:
-                    self.__make_ai_move2(self.__is_x_turn)
+                    self.__make_ai_move3(self.__is_x_turn)
                 else:
                     self.__make_move(int(raw_input("O's turn to move:")) - 1, self.__is_x_turn)
                 self.__is_x_turn = not self.__is_x_turn
@@ -168,6 +170,43 @@ class Game:
                         best_moves.append(i)
             self.__make_move(best_moves[random.randint(0, len(best_moves) - 1)], is_x)
 
+    def __make_ai_move3(self, is_x):
+        if is_x:
+            poss_moves = [{'best': -2, 'avg': -2}] * 9
+            best_poss = -2
+            best_avg = -2
+        else:
+            poss_moves = [{'best': 2, 'avg': 2}] * 9
+            best_poss = 2
+            best_avg = 2
+
+        # loop through the board making all legal moves to assess their strengths
+        # then find the best possible outcome in poss_moves
+        for i in range(0, 9):
+            if self.__is_empty(i):
+                self.__make_move(i, is_x)
+                if is_x:
+                    poss_moves[i] = self.__minimax3(False)
+                    if poss_moves[i]['best'] > best_poss:
+                        best_poss = poss_moves[i]['best']
+                    if poss_moves[i]['avg'] > best_avg:
+                        best_avg = poss_moves[i]['avg']
+                else:
+                    poss_moves[i] = self.__minimax3(True)
+                    if poss_moves[i]['best'] < best_poss:
+                        best_poss = poss_moves[i]['best']
+                    if poss_moves[i]['avg'] < best_avg:
+                        best_avg = poss_moves[i]['avg']
+                self.__revert_move(i)
+
+        best_moves = []
+        # find moves that can reach best outcome level and put the ones with best average outcome in best_moves
+        for i in range(0, 9):
+            if self.__is_empty(i):
+                if poss_moves[i]['best'] == best_poss and float_eq(poss_moves[i]['avg'], best_avg):
+                    best_moves.append(i)
+        self.__make_move(best_moves[random.randint(0, len(best_moves) - 1)], is_x)
+
     def __minimax2(self, is_x):
         # If someone has won, return 1 or -1 depending on who won
         if is_x:
@@ -198,6 +237,35 @@ class Game:
                     their_minimax.append(move_val['best'])
                     self.__revert_move(i)
             return {'best': min(their_minimax), 'avg': sum(their_minimax) / len(their_minimax)}
+
+    def __minimax3(self, is_x):
+        # If someone has won, return 1 or -1 depending on who won
+        if is_x:
+            if self.__someone_won():
+                return {'best': -1, 'avg': -1}
+        else:
+            if self.__someone_won():
+                return {'best': 1, 'avg': 1}
+        if self.__stalemate():
+            return {'best': 0, 'avg': 0}
+
+        their_minimax = []
+        their_avg = []
+        # Recursive calls based on whose turn it is
+        for i in range(0, 9):
+            if self.__id_at(i) == ' ':
+                self.__make_move(i, is_x)
+                if is_x:
+                    move_val = self.__minimax2(False)
+                else:
+                    move_val = self.__minimax2(True)
+                their_minimax.append(move_val['best'])
+                their_avg.append(move_val['avg'])
+                self.__revert_move(i)
+        if is_x:
+            return {'best': max(their_minimax), 'avg': sum(their_avg) / len(their_avg)}
+        else:
+            return {'best': min(their_minimax), 'avg': sum(their_avg) / len(their_avg)}
 
     def set_test(self, num_in, char_in):
         self.__board[num_in // 3][num_in % 3].id = char_in
